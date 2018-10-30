6ef1140e7a603267ca19bde4a14bad15b0646b6a
bhargav-umd <bhargav@umd.edu>
2018-10-30 17:14:17 -0400

    Add comments to talker and listener files

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,14 +1,54 @@
+/* Copyright (C)
+ * 2018 - Bhargav Dandamudi
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+/**
+ * @copyright Copyright 2018 Bhargav Dandamudi
+ * @file listener.cpp
+ * @brief
+ * @author Bhargav Dandamudi
+ * @version 1
+ * @date 2018-10-30
+ */
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+/* --------------------------------------------------------------------------*/
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+ * @brief  Function to read messages and displays it
+ *
+ * @Param msg message recieved from talker node
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+/* --------------------------------------------------------------------------*/
+void chatterCallback(const std_msgs::String::ConstPtr &msg) {
     ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char** argv) {
+/* --------------------------------------------------------------------------*/
+/**
+ * @brief  To create Listener node
+ *
+ * @Param argc is the number of arguments
+ * @Param argv is the arguments
+ *
+ * @Returns 0
+ */
+/* --------------------------------------------------------------------------*/
+int main(int argc, char **argv) {
     /**
      * The ros::init() function needs to see argc and argv so that it can
      * perform
